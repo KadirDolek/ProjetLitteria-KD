33fe23830b163c66674be643709f1c2b8e40e517
kadirdolek <kadirdolek@outlook.com>
2025-07-08 15:07:14 +0200

    collection

--- src/app/collection/page.jsx
+++ src/app/collection/page.jsx
@@ -1,11 +1,10 @@
-'use client'
+"use client";
 
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchBooks } from "../../store/bookSlice";
-import Link from 'next/link';
-import { notFound } from 'next/navigation'
-
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export default function Collection() {
   const dispatch = useDispatch();
@@ -14,7 +13,7 @@ export default function Collection() {
   const [currentPage, setCurrentPage] = React.useState(1);
 
   useEffect(() => {
-    if (status === 'idle') {
+    if (status === "idle") {
       dispatch(fetchBooks());
     }
   }, [dispatch, status]);
@@ -23,15 +22,17 @@ export default function Collection() {
   const startIdx = (currentPage - 1) * itemsPerPage;
   const currentItems = data.slice(startIdx, startIdx + itemsPerPage);
 
-  if (status === 'loading') return <div>Chargement...</div>;
+  if (status === "loading") return <div>Chargement...</div>;
   if (!data.length) return notFound;
 
   return (
-    <section className='h-auto mt-42 text-black'>
-      <h2 className='ps-14 mb-12 text-amber-900 font-bold text-2xl'>Notre collection complète:</h2>
+    <section className="h-auto mt-42 text-black">
+      <h2 className="ps-14 mb-12 text-amber-900 font-bold text-2xl">
+        Notre collection complète:
+      </h2>
 
-      <div className="grid grid-cols-3 gap-8 place-items-center">
-        {currentItems.map(book => (
+      <div className="flex flex-wrap gap-x-64 justify-center">
+        {currentItems.map((book) => (
           <div
             key={book.id}
             className="group flip-card w-64 h-82 relative rounded-3xl"
@@ -39,23 +40,20 @@ export default function Collection() {
             <div className="flip-inner w-full h-full relative">
               {/* Devant */}
               <div className="flip-front bg-transparent rounded-3xl shadow-2xl flex items-center justify-center p-4">
-                
-                    <img
-                  className='rounded-2xl w-48 object-contain'
+                <img
+                  className="rounded-2xl w-48 object-contain"
                   src={book.image_url}
                   alt={book.title}
                 />
-               
-                
               </div>
 
               {/* Derrière */}
               <div className="flip-back bg-transparent rounded-3xl shadow-2xl flex items-center justify-center p-4">
-                <Link href ={`/details/${book.id}`}>
-                <p className="text-black text-sm text-center line-clamp-7 font-bold">
-                  {book.description}
-                </p>
-                 </Link>
+                <Link href={`/details/${book.id}`}>
+                  <p className="text-black text-sm text-center line-clamp-7 font-bold">
+                    {book.description}
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
@@ -65,20 +63,22 @@ export default function Collection() {
       <div className="flex justify-center gap-4 mt-8 mb-12">
         <button
           className="px-3 py-1 bg-transparent disabled:opacity-50 cursor-pointer font-bold"
-          onClick={() => setCurrentPage(p => Math.max(1, p - 1))}
+          onClick={() => setCurrentPage((p) => Math.max(1, p - 1))}
           disabled={currentPage === 1}
         >
           Précédent
         </button>
-        <span className="text-black font-bold">{currentPage} / {totalPages}</span>
+        <span className="text-black font-bold">
+          {currentPage} / {totalPages}
+        </span>
         <button
           className="px-3 py-1 bg-transparent disabled:opacity-50 cursor-pointer font-bold"
-          onClick={() => setCurrentPage(p => Math.min(totalPages, p + 1))}
+          onClick={() => setCurrentPage((p) => Math.min(totalPages, p + 1))}
           disabled={currentPage === totalPages}
         >
           Suivant
         </button>
       </div>
     </section>
-  )
+  );
 }
--- src/app/details/[id]/page.jsx
+++ src/app/details/[id]/page.jsx
@@ -23,11 +23,14 @@ export default function Details() {
   if (!product) return notFound();
 
   return (
-    <div className='flex flex-col items-center justify-center mx-auto w-full h-screen'>
-      <img className='h-80 w-80 rounded-lg' src={product.image_url} alt={product.title} />
+    <div className='flex flex-col items-center justify-center mx-auto w-full h-200 shadow-2xl mb-24 rounded-4xl mt-36'>
+    <h2 className='text-black text-4xl mb-12'>{product.title}</h2>
+      <img className='h-80 w-60 rounded-lg' src={product.image_url} alt={product.title} />
       <hr className='w-90 my-4' />
-      <p>{product.description}</p>
-      <p>{product.rating} ⭐</p>
+      <p className='text-black text-center text-xl px-12 font-bold'>{product.description}</p>
+      <p className='text-black text-2xl mt-12 mb-6'>{product.rating} ⭐</p>
+
+      <button className='border bg-amber-900 cursor-pointer px-8 shadow-2xl border-none rounded-4xl font-bold py-2'>Ajouter au panier</button>
     </div>
   );
 }
